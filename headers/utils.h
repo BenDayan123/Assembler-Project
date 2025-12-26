6571b7d98b8187621eb963e7a0b0f8312596d1e7
Ben Dayan <ben.dayan123@gmail.com>
2025-12-26 17:13:04 +0200

    added missed function header

--- headers/utils.h
+++ headers/utils.h
@@ -6,5 +6,6 @@
 char *convert_to_binary(unsigned int num, unsigned int binary_length);
 char *filename_with_ext(char *filename, const char *ext);
 char *skip_whitespaces(char *str);
+char *handle_malloc(size_t size);
 
 #endif
