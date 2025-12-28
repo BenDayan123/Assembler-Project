d4d61c52b96d52294a8364abc30f86bb637363f6
Ben Dayan <ben.dayan123@gmail.com>
2025-12-28 18:53:13 +0200

    added get_first_word function to get the next word or argument in a string (helpful function)

--- headers/utils.h
+++ headers/utils.h
@@ -2,10 +2,17 @@
 #define UTILS_H
 
 #include <stdio.h>
+#include "../headers/globals.h"
 
 char *convert_to_binary(unsigned int num, unsigned int binary_length);
+CmdInfo *find_cmd_info(char *cmd);
 char *filename_with_ext(char *filename, const char *ext);
+char *join_path_and_ext(char *filename, const char *ext, const char *path);
 char *skip_whitespaces(char *str);
-char *handle_malloc(size_t size);
+void *handle_malloc(size_t size);
+void get_first_word(const char *line, char *word);
+int get_next_word(char **src, char *dest, boolean expect_comma);
+int count_and_validate_data_numbers(char *line);
+int is_register(const char *op);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -63,7 +63,18 @@ char *convert_to_binary(unsigned int num, unsigned int binary_length)
     return binary;
 }
 
-char *handle_malloc(size_t size)
+CmdInfo *find_cmd_info(char *cmd)
+{
+    int i;
+    for (i = 0; operations[i].name != NULL; i++)
+    {
+        if (strcmp(cmd, operations[i].name) == 0)
+            return &operations[i];
+    }
+    return NULL;
+}
+
+void *handle_malloc(size_t size)
 {
     void *ptr = malloc(size);
     if (ptr == NULL)
@@ -73,11 +84,17 @@ char *handle_malloc(size_t size)
 
 char *filename_with_ext(char *filename, const char *ext)
 {
-    char *new_filename = (char *)handle_malloc(strlen(filename) + strlen(ext) + 2);
+    char *new_filename = (char *)handle_malloc(strlen(filename) + strlen(ext) + 1);
     sprintf(new_filename, "%s.%s", filename, ext);
     return new_filename;
 }
 
+char *join_path_and_ext(char *filename, const char *ext, const char *path)
+{
+    char *new_filename = (char *)handle_malloc(strlen(filename) + strlen(ext) + strlen(path) + 1);
+    sprintf(new_filename, "%s/%s.%s", path, filename, ext);
+    return new_filename;
+}
 /*
  * Advances the pointer to skip any leading whitespace (spaces, tabs).
  */
@@ -86,4 +103,120 @@ char *skip_whitespaces(char *str)
     while (*str && isspace(*str))
         str++;
     return str;
+}
+
+void get_first_word(char *line, char *word)
+{
+    int i = 0;
+    int j = 0;
+
+    line = skip_whitespaces(line);
+
+    while (line[i] && !isspace(line[i]))
+        word[j++] = line[i++];
+
+    word[j] = '\0';
+}
+
+int get_next_word(char **src, char *dest, boolean expect_comma)
+{
+    char *curr = *src;
+    int i = 0;
+    if (*curr == '\0')
+        return;
+    memset(dest, '\0', strlen(dest));
+    curr = skip_whitespaces(curr);
+
+    if (expect_comma)
+    {
+        if (*curr == ',')
+        {
+            dest[0] = '\0';
+            *src = (++curr);
+            return -1;
+        }
+        while (*curr && *curr != ',')
+            dest[i++] = *curr++;
+        while (i > 0 && isspace(dest[i - 1]))
+            dest[--i] = '\0';
+        if (*curr == ',')
+            curr++;
+    }
+    else
+    {
+        while (*curr && !isspace(*curr))
+            dest[i++] = *curr++;
+
+        dest[i] = '\0';
+    }
+    *src = curr;
+    return i - 1;
+}
+
+int count_and_validate_data_numbers(char *line)
+{
+    int count = 0;
+    char *ptr = line;
+    int expecting_comma = FALSE;
+
+    ptr = skip_whitespaces(ptr);
+
+    if (*ptr == '\0')
+        return 0;
+
+    while (*ptr != '\0')
+    {
+        if (!expecting_comma)
+        {
+            char *end_ptr;
+            strtol(ptr, &end_ptr, 10);
+            if (*ptr == ',')
+            {
+                printf("Error: Unexpected comma (consecutive or leading).\n");
+                return -1;
+            }
+
+            if (ptr == end_ptr)
+            {
+                printf("Error: Expected a number, found '%c'.\n", *ptr);
+                return -1;
+            }
+
+            count++;
+            ptr = end_ptr;
+            expecting_comma = TRUE;
+        }
+
+        else
+        {
+            ptr = skip_whitespaces(ptr);
+
+            if (*ptr == '\0')
+                break;
+
+            if (*ptr == ',')
+            {
+                ptr++;
+                expecting_comma = FALSE;
+            }
+            else
+            {
+                printf("Error: Missing comma between numbers.\n");
+                return -1;
+            }
+        }
+    }
+
+    if (!expecting_comma)
+    {
+        printf("Error: Trailing comma without a number.\n");
+        return -1;
+    }
+
+    return count;
+}
+
+int is_register(const char *op)
+{
+    return (strlen(op) == 2 && op[0] == 'r' && op[1] >= '0' && op[1] <= '7');
 }
