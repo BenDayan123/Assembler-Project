ec12e005327f59f404b3ad54181c075e16775ca4
Ben Dayan <ben.dayan123@gmail.com>
2025-12-29 01:53:02 +0200

    updated the error management system to have fixed number of error message with same format, and has a red color

--- headers/error.h
+++ headers/error.h
@@ -1,3 +1,48 @@
-#include <stdio.h>
+#ifndef ERRORS_H
+#define ERRORS_H
 
-void print_error(int code);
+typedef enum
+{
+    ERR_NONE = 0,
+
+    /* Syntax */
+    ERR_LINE_TOO_LONG,
+    ERR_SYNTAX_ERROR,
+    ERR_EXTRA_TEXT_AFTER_CMD,
+    ERR_MISSING_COMMA,
+    ERR_MULTIPLE_COMMAS,
+    ERR_COMMA_AT_START,
+    ERR_COMMA_AT_END,
+
+    /* Labels \ Symbols */
+    ERR_LABEL_TOO_LONG,
+    ERR_INVALID_LABEL_NAME,
+    ERR_LABEL_ALREADY_DEFINED,
+    ERR_LABEL_IS_KEYWORD,
+    ERR_UNDEFINED_LABEL,
+    ERR_SYMBOL_ALREADY_DEFINED,
+
+    /* Operations \ Instruction */
+    ERR_UNDEFINED_OPCODE,
+    ERR_UNDEFINED_DIRECTIVE,
+    ERR_MISSING_OPERAND,
+    ERR_TOO_MANY_OPERANDS,
+    ERR_INVALID_OPERAND_TYPE,
+    ERR_UNKNOWN_INSTRUCTION,
+
+    /* Data */
+    ERR_INVALID_NUMBER,
+    ERR_INVALID_STRING,
+    ERR_DATA_OUT_OF_RANGE,
+
+    /* System */
+    ERR_MEMORY_ALLOCATION_FAILED,
+    ERR_OPEN_FILE,
+
+    /* Number of Errors */
+    ERR_COUNT /* NEEDS TO BE IN THE END! */
+} ErrorCode;
+
+void log_error(ErrorCode code, int line_num, const char *filename, const char *info);
+
+#endif
--- src/error.c
+++ src/error.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../headers/error.h"
 
-void print_error(int code)
+#define COLOR_RED "\x1b[31m"
+#define COLOR_RESET "\x1b[0m"
+
+static const char *error_messages[] = {
+    "No error",
+
+    /* Syntax */
+    "Line is too long (max 80 chars)",
+    "General syntax error",
+    "Extraneous text after end of command",
+    "Missing comma between operands",
+    "Multiple consecutive commas",
+    "Line starts with a comma",
+    "Line ends with a comma",
+
+    /* Labels \ Symbols */
+    "Label name exceeds maximum length",
+    "Label name contains invalid characters or format",
+    "Label has already been defined",
+    "Label name cannot be a reserved keyword",
+    "Use of undefined label",
+    "Symbol is already defined in the symbol table",
+
+    /* Operations \ Instructions */
+    "Undefined instruction name",
+    "Undefined directive",
+    "Missing operand for instruction",
+    "Too many operands for instruction",
+    "Operand addressing mode is invalid for this instruction",
+    "Unknown instruction name",
+
+    /* Data */
+    "Expected an integer number",
+    "String must be enclosed in double quotes",
+    "Number is out of range for 12-bit representation",
+
+    /* System */
+    "Fatal: Memory allocation failed",
+    "Could not open file"};
+
+void log_error(ErrorCode code, int line_num, const char *filename, const char *info)
 {
-    printf("Error: Code: %d", code);
+    printf("%s", COLOR_RED);
+    if (line_num > 0)
+        printf("{%s:%d} ", filename, line_num);
+    if (code >= 0 && code < ERR_COUNT)
+        printf("Error: %s", error_messages[code]);
+    else
+        printf("Error: Unknown error code");
+    if (info != NULL)
+        printf(", \"%s\"", info);
+    printf(".%s\n", COLOR_RESET);
 }
