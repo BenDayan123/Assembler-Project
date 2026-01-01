ba7408030112d33ba2002427024a97d1b5346783
Ben Dayan <ben.dayan123@gmail.com>
2026-01-01 16:26:13 +0200

    first main logic (not final)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "../headers/globals.h"
+#include "../headers/utils.h"
+#include "../headers/pre_assembler.h"
+#include "../headers/first_pass.h"
+#include "../headers/second_pass.h"
+#include "../headers/symbol_table.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    SymbolTable *table = NULL;
+    int ICF, DCF;
+    while (--argc > 0)
+    {
+        char *filename = argv[argc];
+        table = create_table();
+
+        run_pre_assembler(filename);
+        run_first_pass(filename, table, &ICF, &DCF);
+        run_second_pass(filename, table, ICF, DCF);
+
+        free_table(table);
+    }
     return 0;
 }
